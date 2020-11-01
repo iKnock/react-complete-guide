83c55631a9e454386b699cd4ff70f4e2e7221fed
iKnock <ergana.henok@gmail.com>
2020-11-01 17:39:21 +0100

    restruct

--- base-my-code/src/Person/Person.js
+++ base-my-code/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.click}>Im {props.name} and i am {props.age} years old</p>
             <p>{props.children}</p>
+            <input type='text' onChange={props.changed}></input>
         </div>
     )
 }
