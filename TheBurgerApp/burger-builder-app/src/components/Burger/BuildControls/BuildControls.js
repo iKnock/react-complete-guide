7458ce4017f4bcb5d648ff780577fd0a9b2a4d7b
iKnock <ergana.henok@gmail.com>
2020-11-10 22:14:45 +0100

    the burger and ingrediant controller

---
+++ TheBurgerApp/burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import BuildControl from './BuildControl/BuildControl'
+import classes from './BuildControls.module.css'
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl
+                added={() => props.ingredientAdded(ctrl.type)}
+                key={ctrl.label}
+                label={ctrl.label} />
+        ))}
+
+    </div>
+);
+
+export default buildControls
--- TheBurgerApp/burger-builder-app/src/components/Burger/Burger.js
+++ TheBurgerApp/burger-builder-app/src/components/Burger/Burger.js
@@ -3,13 +3,29 @@ import React from 'react'
 import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngrediant/BurgerIngrediant'
 
-const burger = (props)=>{
-    return(
+const burger = (props) => {
+
+    //This is how to flat an object to array of jsx element having values of ingredients object
+    let transformedIngredients = Object.keys(props.ingredients)//an array of the key of the object
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length===0){
+        transformedIngredients=<p>Please start adding ingredient</p>
+    }
+
+    return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 }
