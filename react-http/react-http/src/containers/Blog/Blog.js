b527f67a477fb5dc0fbbdcff3c93721a767f8bea
iKnock <ergana.henok@gmail.com>
2020-11-19 19:11:35 +0100

    with router hoc, absolute and relative path

--- react-http/react-http/src/containers/Blog/Blog.js
+++ react-http/react-http/src/containers/Blog/Blog.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 
-import { Route,Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import Posts from './Posts/Posts';
 import NewPost from '../../containers/Blog/NewPost/NewPost'
-import classes from './Blog.module.css';
+import './Blog.css';
 
 class Blog extends Component {
     render() {
         return (
-            <div className={classes.Blog}>
+            <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname:'/new-post',
-                                hash:'#submit',/**to submit as soon as it traversed */
-                                search:'?quick-submit=true'/**to add query param */
-                            }}>New Post</Link></li>
+                            <li><NavLink to="/" exact activeClassName="my_active" activeStyle={{
+                                color: '#fa923f',
+                                textDecoration: 'underline'
+                            }}>Home</NavLink>
+                            </li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',/**to submit as soon as it traversed */
+                                search: '?quick-submit=true'/**to add query param */
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-               { /*<Route path="/" exact render={()=>()} />
+                { /*<Route path="/" exact render={()=>()} />
                 <Route path="/" render={()=><h1>Home2</h1>} />*/}
-                <Route path="/" exact component={Posts}/> 
-                <Route path="/new-post" component={NewPost}/> 
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
-        );  
+        );
     }
 }
 
