49bf53f05f1830bbebc0e4e30cd8ad5ee4fd3cdb
iKnock <ergana.henok@gmail.com>
2020-11-24 21:14:39 +0100

    forms and handling inputs

--- TheBurgerApp/burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ TheBurgerApp/burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -5,39 +5,80 @@ import classes from './ContactData.module.css'
 
 import axios from '../../../axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipcode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
 
     orderHandler = (event) => {
-        console.log("price: " + this.props.price);
-        event.preventDefault();
+        event.preventDefault();//this is to prevent the submit automatically and reloading the page
         //console.log(this.props.ingredients);
         //navigate check out form
         //for now send and save the data to the firestore db
         //Note add .json at the end only for firebase db        
         this.setState({ loading: true });
+        const formDate = {};        
+        for (let formElementIdentifier in this.state.orderForm) {
+            formDate[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
-            customer: {
-                name: 'henok ergana',
-                address: {
-                    street: 'via Addis Abeba 14',
-                    zipcode: '24354',
-                    country: 'Italy'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest',
-            price: this.props.price
+            price: this.props.price,
+            orderData: formDate
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -49,13 +90,40 @@ class ContactData extends Component {
             })
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        //we need to clone the orderForm deeply since it has nested data (immutable update)
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm });
+    }
+
     render() {
+        //this is to flat the orderForm json found in the state to an array called formElementsArray to 
+        //use it for input element dynamically
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
-            <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-                <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>Order Here</Button>
             </form>
         );
