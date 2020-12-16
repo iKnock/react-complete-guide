6370be523215ad98d0de271bb867db7cf2458fbb
iKnock <ergana.henok@gmail.com>
2020-12-16 21:48:52 +0100

    react animation

--- Animation/animations/src/components/Modal/Modal.js
+++ Animation/animations/src/components/Modal/Modal.js
@@ -1,20 +1,35 @@
 import React from "react";
+import Transition from 'react-transition-group/Transition'
 
 import "./Modal.css";
 
 const modal = props => {
-  const cssClasses = [
-    "Modal",
-    props.show ? "ModalOpen" : "ModalClosed"
-  ];
+
+  const animationTiming = {
+    enter: 400,
+    exit: 1000
+  }
+
 
   return (
-    <div className={cssClasses.join(' ')}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
+    <Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
+      {state => {
+        const cssClasses = [
+          "Modal",
+          state === 'entering' ? 'ModalOpen' : state === 'exiting' ? 'ModalClosed' : null
+        ];
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
       </button>
-    </div>
+          </div>
+        );
+
+      }}
+
+    </Transition>
   );
 };
 
